0c2fb69d9922afdc75636b02541b896b926cff31
Horacio Mota <horaciomota93@gmail.com>
2024-03-02 14:12:29 +0000

    Adds bookcar component  and add model for cars

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import Header from "@/Components/Header";
+import BookCar from "@/Components/BookCar";
 import Banner from "@/Components/Banner";
 import ChooseUs from "@/Components/ChooseUs";
 import Download from "@/Components/Download";
@@ -9,6 +10,7 @@ export default function Home() {
     return (
         <>
         <Header/>
+        <BookCar/>
         <Banner/>
         <ChooseUs/>
         <Download/>
